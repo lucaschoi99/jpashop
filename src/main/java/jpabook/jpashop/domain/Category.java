818904d327b91456d940ca1758273e1b2584eb9f
Minsoo Choi <lucas0209@yonsei.ac.kr>
2022-07-07 20:46:36 +0900

    Toy project on JPA Spring Boot

---
+++ src/main/java/jpabook/jpashop/domain/Category.java
@@ -0,0 +1,34 @@
+package jpabook.jpashop.domain;
+
+import jpabook.jpashop.domain.item.Item;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter @Setter
+public class Category {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "category_id")
+    private Long id;
+
+    private String name;
+
+    @OneToMany(mappedBy = "category")
+    private List<CategoryItem> items = new ArrayList<>();
+
+    @ManyToOne
+    @JoinColumn(name = "parent_id")
+    private Category parent;
+
+    @OneToMany(mappedBy = "parent")
+    private List<Category> children = new ArrayList<>();
+
+
+
+}
---
+++ src/main/java/jpabook/jpashop/domain/CategoryItem.java
@@ -0,0 +1,26 @@
+package jpabook.jpashop.domain;
+
+import jpabook.jpashop.domain.item.Item;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter @Setter
+public class CategoryItem {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "categoryitem_id")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "item_id")
+    private Item item;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id")
+    private Category category;
+
+}
