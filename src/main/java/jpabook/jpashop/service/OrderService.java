6245edacb1ee8e545789a7207344ba2dbe92dadd
Minsoo Choi <lucas0209@yonsei.ac.kr>
2022-07-10 14:47:25 +0900

    주문, 취소기능 담당하는 OrderService 개발

---
+++ src/main/java/jpabook/jpashop/service/OrderService.java
@@ -0,0 +1,61 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.Delivery;
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderItem;
+import jpabook.jpashop.domain.item.Item;
+import jpabook.jpashop.repository.ItemRepository;
+import jpabook.jpashop.repository.MemberRepository;
+import jpabook.jpashop.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class OrderService {
+
+    private final OrderRepository orderRepository;
+    private final MemberRepository memberRepository;
+    private final ItemRepository itemRepository;
+
+    // 주문
+    public Long order(Long memberId, Long itemId, int count) {
+
+        // Entity 조회
+        Member member = memberRepository.findMember(memberId);
+        Item item = itemRepository.findItem(itemId);
+
+        // 배송정보 생성
+        Delivery delivery = new Delivery();
+        delivery.setAddress(member.getAddress());
+
+        // 주문상품 생성
+        OrderItem orderItem = OrderItem.createOrderItem(item, item.getPrice(), count);
+
+        // 주문 생성
+        Order order = Order.createOrder(member, delivery, orderItem);
+
+        // 저장 -> order persist 시 cascade로 orderItem, delivery 자동 Persist
+        orderRepository.save(order);
+
+        return order.getId();
+    }
+
+    // 취소
+    public void cancelOrder(Long orderId) {
+        Order order = orderRepository.findOrder(orderId);
+        order.cancel();
+    }
+
+    // 검색
+//    public List<Order> searchOrders(OrderSearch orderSearch) {
+//        return orderRepository.search(orderSearch);
+//    }
+
+
+}
