12e6491bef54092e0b91b577caf956dfd3525aeb
Minsoo Choi <lucas0209@yonsei.ac.kr>
2022-07-22 22:09:48 +0900

    xToMany 연관관계 api 설계에서의 성능 최적화 : Order 뿐만아니라 OrderItem 까지도 Dto로 감싸도록 설계

---
+++ src/main/java/jpabook/jpashop/api/OrderTrickyApiController.java
@@ -0,0 +1,74 @@
+package jpabook.jpashop.api;
+
+import jpabook.jpashop.domain.Address;
+import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderItem;
+import jpabook.jpashop.domain.OrderStatus;
+import jpabook.jpashop.repository.OrderRepository;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * xToMany 관계에서의 성능 최적화
+ * OrderItem
+ * Order -> OrderItem
+ */
+@RestController
+@RequiredArgsConstructor
+public class OrderTrickyApiController {
+
+    private final OrderRepository orderRepository;
+
+    @GetMapping("/api/tricky-orders")
+    public List<TrickyOrderDto> findOrders() {
+        // Fetch join
+        List<Order> orders = orderRepository.findOrdersWithMemDel();
+        return orders.stream()
+                .map(o -> new TrickyOrderDto(o))
+                .collect(Collectors.toList());
+    }
+
+
+    @Data
+    static class TrickyOrderDto {
+
+        private Long orderId;
+        private String name;
+        private LocalDateTime orderDate;
+        private OrderStatus orderStatus;
+        private Address address;
+        private List<OrderItemDto> orderItems;
+
+        public TrickyOrderDto(Order o) {
+            orderId = o.getId();
+            name = o.getMember().getName();
+            orderDate = o.getOrderDate();
+            orderStatus = o.getStatus();
+            address = o.getDelivery().getAddress();
+            orderItems = o.getOrderItems().stream()
+                    .map(orderItem -> new OrderItemDto(orderItem))
+                    .collect(Collectors.toList());
+        }
+    }
+
+    @Data
+    static class OrderItemDto {
+
+        private String itemName;
+        private int orderPrice;
+        private int count;
+
+        public OrderItemDto(OrderItem orderItem) {
+            itemName = orderItem.getItem().getName();
+            orderPrice = orderItem.getOrderPrice();
+            count = orderItem.getCount();
+        }
+    }
+
+}
